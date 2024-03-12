aca6457f30e1297b136a1e0ea0f41f51444b9082
YiheyisL <Yoola7@gmail.com>
2024-03-12 20:20:15 +0300

    api

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-const axiosBase = axios.create({ baseURL: "http://localhost:5100/api" });
-// const axiosBase = axios.create({
-//   // baseURL: "https://evangadi-forum-deploy-2.onrender.com/",
-// });
+// const axiosBase = axios.create({ baseURL: "http://localhost:5100/api" });
+
+const axiosBase = axios.create({
+  baseURL: "https://app.netlify.com/sites/clinquant-crostata-bab01b/deploys/",
+});
 export default axiosBase;
